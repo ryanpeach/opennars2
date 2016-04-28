bcb125d625ed1c1751aa01e113c4edfb15fb8a33
patrick.hammer <patham9@gmail.com>
2016-04-28 12:58:23 +0200

    Rasoms suggestions.

--- src/narjure/term_utils.clj
+++ src/narjure/term_utils.clj
@@ -1,14 +1,16 @@
-(ns narjure.term_utils)
+(ns narjure.term_utils
+  (:require [clojure.set :as set]
+            [narjure.defaults :refer :all]))
 
 (defn interval?                                             
   "Is the term an interval?"
   [content]
-  (and (sequential? content) (= (first content) 'interval)))
+  (and (sequential? content) (= (first content) :interval)))
 
 (defn compound?
   "Is the term a compound term?"
   [content]
-  (and (sequential? content) (not= (first content) 'interval)))
+  (and (sequential? content) (not= (first content) :interval)))
 
 (defn syntactic-complexity
   "Calculates the syntactic complexity of a content term,
@@ -22,9 +24,8 @@
   "Extract the termlink relevant subterms of the term up to 3 levels as demanded by the NAL rules"
   ([level content]
    (if (and (< level 3) (compound? content))
-     (reduce set/union #{content} (map (partial termlink-subterms (+ level 1)) content))
+     (reduce set/union #{content} (map (partial termlink-subterms (inc level)) content))
      #{content}))
   ([content]
-   (filter (fn [z] (and (not (contains? logic-ops z))
-                        (not (interval? z))))
+   (remove #(or (logic-ops %) (interval? %))
            (termlink-subterms 0 content))))
