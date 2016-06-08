44069c42f0d5b2573d418d0bd5e60af7d5b070f5
Tony Lofthouse <tony_lofthouse@btinternet.com>
2016-06-08 09:30:33 +0100

    Control_utils: Removed the recusive make-evidence nonsense as it does not work. Reverted to interleave version which does. See comments and examples in file.

--- src/narjure/control_utils.clj
+++ src/narjure/control_utils.clj
@@ -16,15 +16,27 @@
     (assoc el :priority new-priority
               :id (assoc (:id el) :budget new-budget))))
 
-(defn make-ev-helper [e2 e1 sofar]
-  (let [r1 (first e1)
-        r2 (first e2)]
-    (case [(= nil r1) (= nil r2)]
-      [true true] sofar
-      [true false] (make-ev-helper [] (rest e2) (concat [r2] sofar))
-      [false true] (make-ev-helper (rest e1) [] (concat [r1] sofar))
-      [false false] (make-ev-helper (rest e1) (rest e2) (concat [r1] [r2] sofar)))))
+(comment
+  ; this does not work
+  ; (make-evidence [9 7 5 3 1] [18 16 14 12])
+  ; => (9 18 16 7 5 14 12 3 1)
+
+  (defn make-ev-helper [e2 e1 sofar]
+    (let [r1 (first e1)
+          r2 (first e2)]
+      (case [(= nil r1) (= nil r2)]
+        [true true] sofar
+        [true false] (make-ev-helper [] (rest e2) (concat [r2] sofar))
+        [false true] (make-ev-helper (rest e1) [] (concat [r1] sofar))
+        [false false] (make-ev-helper (rest e1) (rest e2) (concat [r1] [r2] sofar))))))
 
 (def max-evidence 50)
 (defn make-evidence [e1 e2]
   (take max-evidence (reverse (make-ev-helper e1 e2 []))))
+
+; this does work
+; (make-evidence [9 7 5 3 1] [18 16 14 12])
+; => (9 18 7 16 5 14 3 12)
+
+(defn make-evidence [e1 e2]
+  (take max-evidence (interleave e1 e2)))
