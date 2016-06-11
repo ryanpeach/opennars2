99e3802a901ee058b91679fe38699bf62549e490
patrick.hammer <patham9@gmail.com>
2016-06-11 16:09:08 +0200

    LENSE improvement.

--- src/gui/actors.clj
+++ src/gui/actors.clj
@@ -4,37 +4,44 @@
 (def actor-level-width 175)
 (def actor-level-height 100)
 
-(def nodes [{:name :concept-manager :px 0 :py 0}
-            {:name :concepts :px 0 :py 300}
-            {:name :task-dispatcher :px 200 :py 0}
-            {:name :input :px 400 :py -400 :displaysize 10.0} ;-600
-            {:name :sentence-parser :px 400 :py -300}       ;-500
-            {:name :task-creator :px 400 :py -150}
+(def concept-color [200 255 255])
+(def derived-task-color [200 200 255])
+(def task-color [255 255 200])
+(def util-color [200 255 200])
+(def gui-color [240 240 255])
+
+(def nodes [{:name :concept-manager :px 75 :py 0 :backcolor concept-color}
+            {:name :concepts :px -100 :py 300 :backcolor concept-color}
+            {:name :task-dispatcher :px 300 :py 0 :backcolor task-color}
+            {:name :input :px 400 :py -400 :displaysize 10.0 :backcolor gui-color} ;-600
+            {:name :sentence-parser :px 400 :py -300 :backcolor util-color}       ;-500
+            {:name :task-creator :px 400 :py -150 :backcolor util-color}
             ;{:name :input-load-reducer :px 400 :py -325}
-            {:name :operator-executor :px -350 :py -150}
+            {:name :operator-executor :px -100 :py -150 :backcolor [255 200 200]}
             ;{:name :event-buffer :px 200 :py 150}
-            {:name :general-inferencer :px 400 :py 300}
-            {:name :derived-load-reducer :px 400 :py 150}
-            {:name :event-selector :px 600 :py 150}
-            {:name :event-bag :px 775 :py 150}
-            {:name :concept-selector :px 600 :py 450}
-            {:name :concept-bag :px 775 :py 450}
-            {:name :output :px 600 :py -400}])
+            {:name :general-inferencer :px 400 :py 300 :backcolor derived-task-color}
+            {:name :derived-load-reducer :px 400 :py 150 :backcolor util-color}
+            {:name :event-selector :px 600 :py 150 :backcolor task-color}
+            {:name :event-bag :px 775 :py 150 :backcolor task-color}
+            {:name :concept-selector :px -100 :py 450 :backcolor concept-color}
+            {:name :concept-bag :px 75 :py 450 :backcolor concept-color}
+            {:name :output :px 600 :py -400 :backcolor gui-color}])
 
 (def vertices [{:from :concept-manager :to :task-dispatcher :unidirectional false}
-               {:from :concept-manager :to :concepts :unidirectional true}
                ;{:from :task-dispatcher :to :event-buffer :unidirectional true}
                {:from :concepts :to :general-inferencer :unidirectional true}
                {:from :concepts :to :operator-executor :unidirectional true}
                {:from :operator-executor :to :task-creator :unidirectional true}
                {:from :event-selector :to :general-inferencer :unidirectional true}
-               {:from :concept-selector :to :general-inferencer :unidirectional true}
                {:from :sentence-parser :to :task-creator :unidirectional true}
                ;{:from :sentence-parser :to :input-load-reducer}
                ;{:from :input-load-reducer :to :task-creator}
                {:from :general-inferencer :to :derived-load-reducer :unidirectional true}
                {:from :derived-load-reducer :to :task-creator :unidirectional true}
                {:from :task-creator :to :task-dispatcher :unidirectional true}
-               {:from :concept-selector :to :concepts :unidirectional true}])
+               {:from :concept-selector :to :concepts :unidirectional true}
+               {:from :concept-manager :to :concept-bag :unidirectional true}
+               {:from :task-dispatcher :to :concepts :unidirectional true}
+               {:from :task-dispatcher :to :event-bag :unidirectional true}])
 
 (def graph-actors [nodes vertices actor-level-width actor-level-height])
--- src/gui/gui.clj
+++ src/gui/gui.clj
@@ -4,31 +4,32 @@
             [co.paralleluniverse.pulsar.actors :refer [whereis cast!]]
             [narjure.core :refer [start-timers shutdown run stop-timers]]))
 
+(def backcolor [200 200 200])
 (def gui-width 50)
 (def gui-height 25)
 (def inputstr (atom ""))                                    ;py -525 with input load reducer
 (def nodes [{:name :send :px 500 :py -325 :onclick (fn [state]
                                                      (println (str "input narsese " (deref inputstr)))
                                                      (cast! (whereis :sentence-parser) [:narsese-string-msg (deref inputstr)])
-                                                     (swap! inputstr (fn [st] ""))) :backcolor [255 255 255]}
+                                                     (swap! inputstr (fn [st] ""))) :backcolor backcolor}
             {:name :clear :px 400 :py -325 :onclick (fn [state]
-                                                     (swap! inputstr (fn [st] ""))) :backcolor [255 255 255]}
+                                                     (swap! inputstr (fn [st] ""))) :backcolor backcolor}
             {:name :putin :px 450 :py -325 :onclick (fn [state]
                                                       (swap! inputstr (fn [st]
                                                                             (str (input "Add Narsese" :to-string :name) "\n"))))
-             :backcolor [255 255 255]}
+             :backcolor backcolor}
 
             {:name :resume :px 450 :py -525 :onclick (fn [state]
                                                       (start-timers))
-             :backcolor [255 255 255]}
+             :backcolor backcolor}
             {:name :stop :px 500 :py -525 :onclick (fn [state]
                                                        (stop-timers))
-             :backcolor [255 255 255]}
+             :backcolor backcolor}
             {:name :shutdown :px 650 :py -525 :onclick (fn [state]
                                                        (shutdown))
-             :backcolor [255 255 255]}
+             :backcolor backcolor}
             {:name :start :px 700 :py -525 :onclick (fn [state]
                                                        (run))
-             :backcolor [255 255 255]}])
+             :backcolor backcolor}])
 
 (def graph-gui [nodes [] gui-width gui-height])
