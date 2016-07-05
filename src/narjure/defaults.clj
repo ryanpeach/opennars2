4f280947b51ccebd7358a2f26deca54a3b3e8f42
patham9 <patham9@gmail.com>
2016-07-05 20:38:00 +0000

    better settings.

--- src/narjure/defaults.clj
+++ src/narjure/defaults.clj
@@ -57,8 +57,8 @@
 ; decay-rate of 10 would fully decay it in 100 cycles
 (def system-tick-interval-slow 150)
 (def inference-tick-interval-slow 110)
-(def system-tick-interval-fast 20)
-(def inference-tick-interval-fast 10)
+(def system-tick-interval-fast 50)
+(def inference-tick-interval-fast 25)
 (def system-tick-interval (atom system-tick-interval-slow))                               ;make big enough
 (def inference-tick-interval (atom inference-tick-interval-slow))
 (def anticipation-scale-dependent-tolerance 4.0)            ;has to be 4 since interval rounding has to agree with time measurement in 2-power
