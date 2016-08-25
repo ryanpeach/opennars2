faed77e43a068ae7e0c36c550d8ea7dbaa60a6d0
Ryan Peach <rgpeach10@gmail.com>
2016-08-25 17:42:37 +0000

    Updated the help doc and formalized the features, untested.

--- src/examples/interface.clj
+++ src/examples/interface.clj
@@ -10,26 +10,28 @@
   (:gen-class))
 
 ; The help dialogue for the server
-(def HELP "Client Querys:
-!new_op {string} - name a new operation
-!n {string} - input narsese.
-!c {string} - show concept.
-!cs - show concepts.
-!r - reset NARS.
+(def predefined-ops ["new-op" "input" "valid" "concept" "concepts"
+                     "help" "reset" "quit" "answer" "say" CONFIRMED INVALID IN OUT])
+(def HELP 
+"
+        Client Querys                       |         Expected Reply
+       op    |       args                   |     op     |          args
+1.  new-op   | opname1 opname2...           | new-op     | success1? success2?... 
+2.  input    | narsese1 narsese2...         | input      | success1? success2?...
+3.  valid    | narsese1 narsese2...         | valid      | tf1 tf2...
+4.  concept  | narsese1 narsese2...         | concept    | conceptstr1/invalid conceptstr2/invalid... 
+5.  concepts |                              | concept    | conceptstr1 conceptstr2...
+6.  help     |                              | help       | helpstring
+7.  reset    |                              | confirmed? |
+8.  quit     |                              | confirmed? |
+9.  answer   | success? return1 return2...  | confirmed? |
 
-Server Response:
-*answ {question} {answer}
-*invalid
-*confirmed
-*online
-*offline
-
-Server Commands:
-?say &args - execute operation
-
-Client Response:
-$op_name $t/f$ & ret
-$invalid")
+        Server Querys                       |         Expected Reply                   
+       op    |       args                   |     op     |          args
+9.  op-name  | arg1 arg2...                 | answer     | success? return1 return2... 
+10. say      | narsese                      |            |
+11. answer   | task solution                |            |
+")
 
 ; ID & Dividers
 (defn newid [] (str (java.util.UUID/randomUUID)))
@@ -39,18 +41,10 @@ $invalid")
 (def INVALID "invalid")
 
 ; Support functions
-(defn exists?
-  "Tests: v equals some item in l?"
-  [v l]
-  (some #(= v %) l))
-
-(defn all?
-  "[l]:   Tests: All items in l are true.
-   [f l]: Tests: All items in l are evaluated true by f?"
-  ([l]
-  (all? (fn [x] x) l))
-  ([f l]
-  (not (some (complement f) l))))
+(defn replace-with
+  [l f y]
+  (let [r (fn [x] (if (f x) y x))] ; if f(x) is true, then replace with y, else return x
+    (map r l)))                    ; return the mapping of r onto l
 
 ; Communication
 (def writer (chan))
@@ -81,7 +75,7 @@ $invalid")
 (def conf  #(confirm % true))
 
 ; Narsese Input
-(defn parse-narsese
+(defn input-narsese
   "Input the received Narsese into the system."
   [string]
   (try
@@ -105,7 +99,7 @@ $invalid")
   [op_name args operationgoal]
   ; First, create the key we will use for this particular call
   (let [id (newid)
-        comb (into args operationgoal)]
+        comb (conj [operationgoal] args)]
   ; First, add a channel for yourself
   (swap! waiting conj [id (chan)])
   ; Then, send the message requesting an answer
@@ -115,20 +109,30 @@ $invalid")
   ; When done, delete yourself from waiting
   (swap! waiting dissoc id)
   ; Then, process extra as narsee, and return true or false
-  (if (exists? tf ["True" "true" "t" "T" "1" "1." "1.0"])
-      (confirm id (all? (map parse-narsese extra)))
-      false))))
+  ()
 
-(defn new_op
+(defn new-op
   "Register a new operation."
   [op_name]
-  (do (nars-register-operation (partial new_op_template op_name)) true))
+  (if (not (some #{op_name} predefined-ops))
+    (do 
+      (nars-register-operation (partial new_op_template op_name))
+      true)
+    (false)))
+
+(defn string-to-bool
+  "Converts to true if it matches certain templates, false otherwise."
+  [tfstr]
+  (some #{tfstr} ["True" "true" "t" "T" "1" "1." "1.0"]))
 
 (defn answer-question
   "Specifically handles answers."
-  [id tf & extra]
-  (>!! (get waiting id) (into [tf] extra))) ; put [tf & extra] onto channel in waiting at key id
-
+  [id tfstr & args]
+  (let [tf (string-to-bool tfstr)]
+  (if (not (every? identity (map valid args)))
+    (error id)
+    (do (>!! (get waiting id) (into [tf] args)) true))))
+  
 ; Copied from ircbot
 (defn concept
   "Show a concept"
@@ -138,63 +142,81 @@ $invalid")
       (dissoc
         (first (narjure.bag/get-by-id @c-bag (:statement statement)))
         :ref))
-    (catch Exception e (str "Invalid narsese " concept-str))))
+    (catch Exception e nil)))
 
 (defn concepts
   "Show all the concepts"
-  []
-  (:priority-index @c-bag))
-
+  ([]
+  (:priority-index @c-bag))              ; get all concepts. FIXME: Is this a list?
+  ([args]
+  (let [returns (map concept args)]      ; get a list containing each concept or an error
+    (replace-with returns not INVALID))) ; replace errors with INVALID and return 
+  ([& args] (concepts args)))            ; can take them either as a list or as a bunch of items
+  
 ; Read Loop
+(def running (atom true))
+(defn quit
+  "Quits everything"
+  []
+  (nar/shutdown)
+  (swap! running not)
+  true)
+
+(defn reset-nars
+  "Resets the system"
+  []
+  (nar/shutdown)
+  (nar/run)
+  true)
+
 (defn parse-in
   "Prints the string as received and splits it in two."
-  [string] (do
+  [string]
     (println (str "Received: " string))
-    (map cstr/trim (cstr/split string (re-pattern IN)))))
+    (map cstr/trim (cstr/split string (re-pattern IN))))
 
 (defn process-in
   [id op & args]
   (case op
-    "new-op"   (confirm id (all? (map new_op args)))
-    "answer"   (confirm id (apply answer-question (into [id] args)))
-    "input"    (confirm id (all? (map parse-narsese args)))
-    "concept"  (try (sendCMD id "concept" (concept (get args 0))) (catch Exception e (error id)))
-    "concepts" (sendCMD id "concepts" (concepts))
-    "help"     (sendCMD id (str HELP))
-    "valid"    (confirm id (all? (map valid-narsese args)))
+    "new-op"     (sendCMD (into [id "new-op"] (map new-op args)))
+    "input"      (sendCMD (into [id "input"] (map input-narsese args)))
+    "valid"      (sendCMD (into [id "valid"] (map valid-narsese args)))
+    "concept"    (sendCMD (into [id "concept"] (concepts args)))
+    "concepts"   (sendCMD (into [id "concept"] (concepts)))
+    "help"       (sendCMD id "help" (str HELP))
+    "reset"      (confirm id (reset-nars))
+    "quit"       (confirm id (quit))
+    "answer"     (confirm id (apply answer-question (into [id] args)))
     (error id)))
 
 (def reader (chan))
 (defn readCMD
   "The main reading loop."
   []
-  (do
-    (let [input (<!! reader)]
-      (println (str "Received: " input))
-      (apply process-in (parse-in input))
-    (recur))))
+  (let [input (<!! reader)]
+    (println (str "Received: " input))
+    (apply process-in (parse-in input)))
+  (if (@running) (recur) (nil)))
 
 ; Startup
 (defn setup-nars
   "Registers the operation and answer handler"
   []
-  (do
   (nars-register-operation 'op_say (fn [args operationgoal]
-                                      (let [allargs (conj args operationgoal)
+                                      (let [allargs (into [operationgoal] args)
                                             total   (into [(newid) "say"] allargs)]
                                       (apply sendCMD total))))
   (nars-register-answer-handler (fn [task solution]
-                                  (let [taskn (str (narsese-print (:statement task)) "?")
+                                  (let [taskn (str (narsese-print (:statement task)))
                                         soln  (str (task-to-narsese solution))]
-                                    (sendCMD (newid) "answer" taskn soln))))
-  ))
+                                    (sendCMD (newid) "answer" taskn soln)))))
 
-(defn -test [] (do
+(defn -test
+  []
   (>!! reader (str 1 IN "input" IN "<a --> b>."))
   (>!! reader (str 2 IN "input" IN "<b --> c>."))
   (>!! reader (str 3 IN "input" IN "<a --> c>?"))
-  (loop [] (println (<!! writer)) (recur))
-  ))
+  (loop [] (println (<!! writer)) (recur)))
 
 (defn -main [& args] (do
   ;(println "Connecting...")
